e0312fc9384eaee2f0a1cb0d281f5e6650e1c9fc
decoyinkot <55494828+decoyinkot@users.noreply.github.com>
2019-11-19 17:19:04 +0300

    Add index.htmml

---
+++ Cinema/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#filter").keyup(function(){
+ 
+        // Retrieve the input field text and reset the count to zero
+        var filter = $(this).val(), count = 0;
+ 
+        // Loop through the comment list
+        $("div ul li").each(function(){
+ 
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut();
+ 
+            // Show the list item if the phrase matches and increase the count by 1
+            } else {
+                $(this).show();
+                count++;
+            }
+        });
+ 
+        // Update the count
+        var numberItems = count;
+        $("#filter-count").text("Найдено = "+count);
+    });
+});
+
+$(function () { 
+    $('.button-container a').each(function () {
+        var location = window.location.href;
+        var link = this.href; 
+        if(location == link) {
+            $(this).addClass('active');
+        }
+    });
+});
